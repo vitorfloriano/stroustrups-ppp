1002e46d6bd6f696cf343f7a695972dded4cbc0c
vfc <107767584+vitorfloriano@users.noreply.github.com>
2024-06-28 11:43:19 -0300

    First commit with some chokeful of changes \n Behold the C++

---
+++ 2.1.cpp
@@ -0,0 +1,12 @@
+// read and write a first name
+using namespace std;
+#include <iostream>
+
+int main()
+{
+	cout << "Please enter your first name (followed by 'enter'):\n";
+	string first_name;
+	cin >> first_name;
+	cout << "Hello, " << first_name << "!\n";
+	cout << "first_name" << " is " << first_name << "\n";
+}
---
+++ 2.2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int number_of_steps = 39;
+	double flying_time = 3.5;
+	char decimal_point = '.';
+	string name = "Annemarie";
+	bool tap_on = true;
+
+	cout << number_of_steps << "\n" << flying_time << "\n" << decimal_point << "\n" << name << "\n" << tap_on << "\n";
+	
+	return 0;
+}
