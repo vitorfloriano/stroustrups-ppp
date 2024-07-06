1347699cfb386715a5d45234b7a5863d229878dc
vitor <107767584+vitorfloriano@users.noreply.github.com>
2024-07-05 23:57:23 -0300

    Add another example from the book
    This time, not as important as the others before.

---
+++ 2.7.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 7;
+	int b = 9;	
+	char c = 'a'; 
+	double x = 1.2;
+	string s1 = "Hello, World!";
+      	string s2 = "1.2";
+}
