1048e2ef2b4ec51ad4c8884c8c45b3965d89d2d2
vitor <107767584+vitorfloriano@users.noreply.github.com>
2024-07-10 23:46:52 -0300

    Add another example from the book
    This time about converting ints to chars and vice versa.

---
+++ 2.9.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+char c = 'x';
+int i1 = c;
+int i2 = c + 1000;
+double d = i2 + 7.3;
+
+double x = 2.7;
+int y = x;
+
+int main()
+{
+	double d = 0;
+	while (cin>>d) {
+		int i = d;
+		char c = i;
+		cout << "d==" << d
+			<< " i==" << i
+			<< " c==" << c
+			<< " char(" << c << ")\n";
+	}
+}
