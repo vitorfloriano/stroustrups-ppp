322a8a3a1a16e9f56dbbf4621f737ebf9332db96
vitor <107767584+vitorfloriano@users.noreply.github.com>
2024-07-12 23:59:01 -0300

    Add variable initialization examples for 2.9
    This adds a few examples of how you can actually initialize a variable in C++. Some of them prevent narrowing when promoting a variable to another type.

--- 2.9.cpp
+++ 2.9.cpp
@@ -21,3 +21,15 @@ int main()
 			<< " char(" << c << ")\n";
 	}
 }
+
+int x= 7.8;
+int x1 {7.8};
+int x2 = {7.8};
+int x3 (7.8);
+
+int x = 7;
+double d = 7.7;
+string s = "Hello, World\n";
+
+vector v = {1,2,3,5,8};
+pair p {"Hello",17};
