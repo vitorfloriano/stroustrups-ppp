17fe229e434ca57070babc4a81cee0cd595bd1f9
vfc <107767584+vitorfloriano@users.noreply.github.com>
2024-07-15 10:42:52 -0300

    Add the first part of the drill.
    I never though it could be that hard to write something so simple,
    but I won't give up.

---
+++ chapter-02/drill.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Enter the name of the person you want to write to: ";
+	string first_name;
+	cin >> first_name;
+	cout << "Hello, " << first_name << "\n";
+}
