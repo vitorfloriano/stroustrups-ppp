b8703e89f167d71b8e6d2d77224000e9935d99a2
vitor <107767584+vitorfloriano@users.noreply.github.com>
2024-07-24 19:02:29 -0300

    Fix 3.6.4.2
    This fixes the exercises in TRY THIS section of 3.6.4 in PPP

--- chapter-03/3.6.4.2.cpp
+++ chapter-03/3.6.4.2.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-	std::string dislikes = "bolsonaro";
+	std::vector<std::string> dislikes = {"bolsonaro","michele","queiroz"};
 	std::vector<std::string> words;
 
 	for (std::string temp; std::cin >> temp;)
@@ -15,13 +15,13 @@ int main()
 
 	for (int i = 0; i < words.size(); ++i)
 	{
-		if (words[i] != dislikes)
+		if (std::find(dislikes.begin(),dislikes.end(),words[i])!=dislikes.end())
 		{
-			std::cout << words[i] << std::endl;
+			std::cout << "BLEEP" << std::endl;
 		}
 		else
 		{
-			std::cout << "BLEEP" << std::endl;
+			std::cout << words[i] << std::endl;
 		}
 	}
 
