d2587953b6fbd86223175d9a7826e69bea34bb27
vfc <107767584+vitorfloriano@users.noreply.github.com>
2024-07-29 10:32:37 -0300

    Fix drill 7
    Ok I think I'm done with it now

--- chapter-03/drill.7.cpp
+++ chapter-03/drill.7.cpp
@@ -23,23 +23,7 @@ int main()
 		} else {
 			std::cout << "Illegal" << std::endl;
 		} 
-
-		if (firstInput) {
-			smallest = a;
-			largest = a;
-			firstInput = false;
-		}
-		
-		if (a <= smallest) {
-			smallest = a;
-			std::cout << "The smallest so far" << std::endl;	
-		}
-
-		if (a > largest) {
-			largest = a;
-			std::cout << "The largest so far" << std::endl;
-		}
-	}	
+	}
 	
 	return 0;
 }
