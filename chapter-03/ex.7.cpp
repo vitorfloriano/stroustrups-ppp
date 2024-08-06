b13bba770c2aa1521ee25fe7f395483ef9f5be33
vfc <107767584+vitorfloriano@users.noreply.github.com>
2024-08-06 16:02:41 -0300

    Do something with exercise 7 Honestly, I give up

--- chapter-03/ex.7.cpp
+++ chapter-03/ex.7.cpp
@@ -8,8 +8,35 @@ void checkInput() {
 	int userInput;
 	while (std::cin >> userInput) {
 		switch (userInput) {
+		case 0:
+			std::cout << "zero" << std::endl;
+			break;
 		case 1:
 			std::cout << "one" << std::endl;
+			break;
+		case 2:
+			std::cout << "two" << std::endl;
+			break;
+		case 3:
+			std::cout << "three" << std::endl;
+			break;
+		case 4:
+			std::cout << "four" << std::endl;
+			break;
+		case 5:
+			std::cout << "five" << std::endl;
+			break;
+		case 6:
+			std::cout << "six" << std::endl;
+			break;
+		case 7:
+			std::cout << "seven" << std::endl;
+			break;
+		case 8:
+			std::cout << "eight" << std::endl;
+			break;
+		case 9:
+			std::cout << "nine" << std::endl;
 			break;
 		}
 	}
